4dbc0a68d3b3625fc44e74e6d001b0e926d7b6bb
yashu sharma <ys4531104@gmail.com>
2026-03-21 22:34:25 +0530

    Update Arrow Function.js

--- Arrow Function.js
+++ Arrow Function.js
@@ -1,3 +1,9 @@
+1 What are arrow functions
+
+Arrow Functions introduced in ES6 and 
+Arrow Functions allow a shorter syntax
+An arrow function is always written as a function expression.
+
 //  1 Basic Example:
 //  const greet = () => {
 //   console.log("Hello from an anonymous arrow function!");
