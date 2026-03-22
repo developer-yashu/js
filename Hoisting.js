6370da2e7203299cc48506ff04726daf33754373
yashu sharma <ys4531104@gmail.com>
2026-03-22 13:50:44 +0530

    Update Hoisting.js

--- Hoisting.js
+++ Hoisting.js
@@ -1,2 +1,7 @@
- 
+What is hoisting in JavaScript ?
+
+ Hoisting is JavaScript’s behavior of moving declarations to the top of the current scope (function or global).
+
+console.log(a); // undefined
+var a = 5;
 
