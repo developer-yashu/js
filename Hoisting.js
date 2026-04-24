20653027bbb1e67cc1373ceb471bcd31b34b05d0
yashu sharma <ys4531104@gmail.com>
2026-04-24 19:07:46 +0530

    Update Hoisting.js

--- Hoisting.js
+++ Hoisting.js
@@ -2,6 +2,11 @@ What is hoisting in JavaScript ?
 
  Hoisting is JavaScript’s behavior of moving declarations to the top of the current scope (function or global).
 
+ 
 console.log(a); // undefined
 var a = 5;
 
+
+let or const 
+Ye Temporal Dead Zone (TDZ) me rehte hain (declare hone tak access nahi kar sakte)
+Access karoge → ReferenceError
