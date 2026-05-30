7255a137da865f2ecff0a1be471e0a113b71570b
yashu sharma <ys4531104@gmail.com>
2026-05-30 22:12:00 +0530

    Update js coding.js

--- js coding.js
+++ js coding.js
@@ -301,7 +301,36 @@ Example: [1,2,2,3,3,3] → {1:1,2:2,3:3}.*/
 
 
 
+// 31 array short 
+// 1 logic
+let arr = [5, 2, 8, 1, 3];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+
+console.log(arr); // [1, 2, 3, 5, 8]
 
 
+2 
+let arr = [5, 2, 8, 1, 3];
+for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[min]) {
+            min = j;
+        }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+}
+console.log(arr); // [1, 2, 3, 5, 8]
 
 
